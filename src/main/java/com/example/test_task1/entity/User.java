08bd929a68cd8501b10396b2f846ef6c3ec4ec54
Stonewall1 <impression97p@gmail.com>
2022-11-28 20:57:48 +0300

    User added

--- src/main/java/com/example/test_task1/entity/User.java
+++ src/main/java/com/example/test_task1/entity/User.java
@@ -1,4 +1,18 @@
 package com.example.test_task1.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
 }
